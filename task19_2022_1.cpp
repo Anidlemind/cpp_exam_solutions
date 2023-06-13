7f706059de1781683fbc125ee8f74095b6ab6815
Arslan223 <agawchik@mail.ru>
2023-06-13 09:22:01 +0300

    Add code for task19_2022_1.cpp

--- task19_2022_1.cpp
+++ task19_2022_1.cpp
@@ -1,1 +1,9 @@
-// enable_if // need to see some lectures
+template <bool B, typename T = void> struct enable_if {};
+
+template <typename T>
+struct enable_if<true, T> {
+  using type = T;
+};
+
+template< bool B, class T = void >
+using enable_if_t = typename enable_if <B,T>::type;
