3972cf8b56a1373d5a0170e94d701bea611559b7
Arslan223 <agawchik@mail.ru>
2023-06-13 09:19:02 +0300

    Add code for task21_2022_1.cpp

--- task21_2022_1.cpp
+++ task21_2022_1.cpp
@@ -1,1 +1,9 @@
-// metafunctions again! // lectures!
+template <typename TP, typename T, class... Types>
+struct is_any_of {
+  static const bool value = std::is_same<TP, T>() or is_any_of<TP, Types...>::value;
+};
+
+template <typename TP, typename T>
+struct is_any_of<TP, T> {
+  static const bool value = std::is_same<TP, T>();
+};
